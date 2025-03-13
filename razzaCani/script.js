b07799de690fe462fb27ac801993689f71a34956
lai2-24 <lai2-24@itismeucci.com>
2025-03-13 14:25:13 +0100

    razza_cani

--- razzaCani/script.js
+++ razzaCani/script.js
@@ -1,53 +1,55 @@
-
 const breedSelect = document.getElementById('breed-select');
 const imagesContainer = document.getElementById('dog-images-container');
 
-function fetchBreeds() {
-    fetch('https://dog.ceo/api/breeds/list/all')
-        .then(response => response.json())
-        .then(data => {
-            const breeds = data.message;
+async function fetchBreeds() {
+    try {
+        const response = await fetch('https://dog.ceo/api/breeds/list/all');
+        const data = await response.json();
+        const breeds = data.message;
 
-            for (let breed in breeds) {
-                const option = document.createElement('option');
-                option.value = breed;
-                option.textContent = breed.charAt(0).toUpperCase() + breed.slice(1);
-                breedSelect.appendChild(option);
-            }
-        })
-        .catch(error => console.error('Errore ', error));
+        for (let breed in breeds) {
+            const option = document.createElement('option');
+            option.value = breed;
+            option.textContent = breed.charAt(0).toUpperCase() + breed.slice(1);
+            breedSelect.appendChild(option);
+        }
+    } catch (error) {
+        console.error('Errore ', error);
+    }
 }
 
-function fetchImagesPerBreed(breed) {
-    fetch(`https://dog.ceo/api/breed/${breed}/images`)
-        .then(response => response.json())
-        .then(data => {
-            imagesContainer.innerHTML = '';
+async function fetchImagesPerBreed(breed) {
+    try {
+        const response = await fetch(`https://dog.ceo/api/breed/${breed}/images`);
+        const data = await response.json();
 
-            if (data.message.length > 0) {
-                data.message.forEach(imgUrl => {
-                    const card = document.createElement('div');
-                    card.classList.add('card');
+        imagesContainer.innerHTML = '';
 
-                    const img = document.createElement('img');
-                    img.src = imgUrl;
-                    img.classList.add('card-img-top');
+        if (data.message.length > 0) {
+            data.message.forEach(imgUrl => {
+                const card = document.createElement('div');
+                card.classList.add('card');
 
-                    const cardBody = document.createElement('div');
-                    cardBody.classList.add('card-body');
+                const img = document.createElement('img');
+                img.src = imgUrl;
+                img.classList.add('card-img-top');
 
-                    card.appendChild(img);
-                    card.appendChild(cardBody);
-                    imagesContainer.appendChild(card);
-                });
-            } else {
-                //se non c'e' img
-                const noImagesMessage = document.createElement('p');
-                noImagesMessage.textContent = `niente img ${breed}.`;
-                imagesContainer.appendChild(noImagesMessage);
-            }
-        })
-        .catch(error => console.error('Errore ', error));
+                const cardBody = document.createElement('div');
+                cardBody.classList.add('card-body');
+
+                card.appendChild(img);
+                card.appendChild(cardBody);
+                imagesContainer.appendChild(card);
+            });
+        } else {
+            // se non c'e' img
+            const noImagesMessage = document.createElement('p');
+            noImagesMessage.textContent = `Niente img per ${breed}.`;
+            imagesContainer.appendChild(noImagesMessage);
+        }
+    } catch (error) {
+        console.error('Errore ', error);
+    }
 }
 
 breedSelect.addEventListener('change', function() {
